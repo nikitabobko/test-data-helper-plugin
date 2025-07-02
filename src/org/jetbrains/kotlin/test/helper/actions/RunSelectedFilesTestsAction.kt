17ee616aaef891f8f773de5fe1f92ed353431f2f
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-07-02 10:04:24 +0200

    Hide run selected action group when no actions are available

--- src/org/jetbrains/kotlin/test/helper/actions/RunSelectedFilesTestsAction.kt
+++ src/org/jetbrains/kotlin/test/helper/actions/RunSelectedFilesTestsAction.kt
@@ -4,16 +4,27 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.components.service
 import org.jetbrains.kotlin.test.helper.getTestDataType
 import org.jetbrains.kotlin.test.helper.isGradleEnabled
 import org.jetbrains.kotlin.test.helper.services.TestDataRunnerService
 
+class RunSelectedFilesGroup : DefaultActionGroup() {
+    override fun update(e: AnActionEvent) {
+        e.updateRunSelectedPresentation()
+        super.update(e)
+    }
+
+    override fun getActionUpdateThread(): ActionUpdateThread {
+        return ActionUpdateThread.BGT
+    }
+}
+
 abstract class RunSelectedFilesActionBase : AnAction() {
     override fun update(e: AnActionEvent) {
-        val project = e.project ?: return
-        val selectedFiles = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY) ?: emptyArray()
-        e.presentation.isEnabledAndVisible = selectedFiles.any { it.getTestDataType(project) != null } && project.isGradleEnabled()
+        e.updateRunSelectedPresentation()
+        return
     }
 
     override fun getActionUpdateThread(): ActionUpdateThread {
@@ -21,6 +32,13 @@ abstract class RunSelectedFilesActionBase : AnAction() {
     }
 
     abstract val isDebug: Boolean
+}
+
+private fun AnActionEvent.updateRunSelectedPresentation() {
+    val project = this.project ?: return
+    val selectedFiles = getData(CommonDataKeys.VIRTUAL_FILE_ARRAY) ?: emptyArray()
+    presentation.isEnabledAndVisible =
+        selectedFiles.any { it.getTestDataType(project) != null } && project.isGradleEnabled()
 }
 
 abstract class AbstractRunSelectedFilesTestsAction : RunSelectedFilesActionBase() {
