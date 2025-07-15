8e19d27dc6434b5d1399387e8051e6b98c294403
Kirill Rakhman <kirill.rakhman@gmail.com>
2025-07-15 10:27:39 +0200

    Insert ticket number in test files in create reproducer action

--- src/org/jetbrains/kotlin/test/helper/actions/CreateReproducerCommitAction.kt
+++ src/org/jetbrains/kotlin/test/helper/actions/CreateReproducerCommitAction.kt
@@ -5,10 +5,13 @@ import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.ex.ActionUtil
+import com.intellij.openapi.command.writeCommandAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vcs.changes.actions.RefreshAction
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.findDocument
 import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.platform.util.progress.reportSequentialProgress
 import git4idea.commands.Git
@@ -16,7 +19,9 @@ import git4idea.commands.GitCommand
 import git4idea.commands.GitLineHandler
 import git4idea.repo.GitRepositoryManager
 import kotlinx.coroutines.launch
+import org.jetbrains.kotlin.test.helper.TestDataType
 import org.jetbrains.kotlin.test.helper.generateTestsAndWait
+import org.jetbrains.kotlin.test.helper.getTestDataType
 import org.jetbrains.kotlin.test.helper.services.TestDataRunnerService
 
 class CreateReproducerCommitAction : RunSelectedFilesActionBase() {
@@ -35,7 +40,12 @@ class CreateReproducerCommitAction : RunSelectedFilesActionBase() {
         service.scope.launch {
             withBackgroundProgress(project, "Creating Reproducer Commit for $ticketNumber") {
                 reportSequentialProgress { reporter ->
-                    reporter.nextStep(25)
+                    reporter.nextStep(25) {
+                        for (file in files) {
+                            file.insertTicketNumberIfNecessary(ticketNumber, project)
+                        }
+                    }
+
                     reporter.nextStep(50, "Generating Tests") {
                         generateTestsAndWait(project, files)
                     }
@@ -60,6 +70,23 @@ class CreateReproducerCommitAction : RunSelectedFilesActionBase() {
 
     override val isDebug: Boolean
         get() = false
+
+    private suspend fun VirtualFile.insertTicketNumberIfNecessary(ticketNumber: String, project: Project) {
+        if (this.isDirectory || getTestDataType(project) != TestDataType.File) {
+            return
+        }
+
+        @Suppress("UnstableApiUsage")
+        writeCommandAction(project, "Adding Ticket Number") {
+            val document = findDocument() ?: return@writeCommandAction
+
+            if (document.text.lineSequence().any { it.startsWith("// ISSUE") && it.contains(ticketNumber) }) {
+                return@writeCommandAction
+            }
+
+            document.insertString(0, "// ISSUE: $ticketNumber\n")
+        }
+    }
 
     private fun commitAll(project: Project, ticketNumber: String) {
         val repository = GitRepositoryManager.getInstance(project).repositories.firstOrNull()
